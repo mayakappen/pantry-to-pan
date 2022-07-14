02b299697af825db9e923ef6a5792371249dffed
Maya Kappen <mayakappen@gmail.com>
2022-07-14 00:06:44 -0600

    Finish User-test and User-class

--- src/classes/User-class.js
+++ src/classes/User-class.js
@@ -1,9 +1,19 @@
+import Recipe from "./Recipe"
+
 class User {
   constructor(newUser) {
-    this.newUser = newUser;
+    this.name = newUser.name
+    this.id = newUser.id
+    this.pantry = newUser.pantry
+    this.toCook = []
   }
-  recipeToCook() {}
-  deleteRecipeToCook() {}
+  recipeToCook(recipe) {
+    this.toCook.push(recipe)
+    return this.toCook
+  }
+  removeRecipeToCook(recipe) {
+    return this.toCook.pop(recipe)
+  }
 }
 
 export default User;
