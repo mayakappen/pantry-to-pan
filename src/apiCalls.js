a88c75d584a6cfbfe2a8cbe184a9ebdb5063a6d6
William Hobson <will.hobson85@gmail.com>
2022-07-18 09:59:31 -0400

    Wrote script for fetch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,12 @@
 // Your fetch requests will live here!
+// const userAPIData = fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users').then(response => response.json()).catch(error => console.log(error));
+// const IngredientAPIData = fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients').then(response => response.json()).catch(error => console.log(error));
+// const recpieAPIData = fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes').then(response => response.json()).catch(error => console.log(error));
 
+// export {
+//     userAPIData,
+//     ingredientAPIData,
+//     recipeAPIData,
+// }
 
 console.log('I will be a fetch request!')
