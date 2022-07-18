d4a53a3168babda1b0473aaf4875680d44e3e74a
Maya Kappen <mayakappen@gmail.com>
2022-07-18 10:45:31 -0600

    get checkbox values and return filtered

--- src/scripts.js
+++ src/scripts.js
@@ -10,7 +10,7 @@ import Recipe from "../src/classes/Recipe";
 //import "../data/ingredients.js";
 
 // 👇🏽 Global variables 👇🏽
-let recipeRepo = new RecipeRepository(recipeData);
+var recipeRepo = new RecipeRepository(recipeData);
 
 // let newRecipe = new Recipe(recipeData)
 // console.log(newRecipe)
@@ -37,10 +37,12 @@ const allRecipeGrid = document.querySelector(".recipe-tile-grid");
 const recipeTile = document.querySelector(".recipe-tile");
 const tileImage = document.getElementById("tileImage");
 const recipePage = document.querySelector(".recipe-page");
+const checkboxes = document.querySelectorAll("input[type='checkbox']");
 
 const breakfastCategory = document.getElementById("breakfast"); //ln 24-breakfast panel on home
 const lunchCategory = document.getElementById("lunch");
 const dinnerCategory = document.getElementById("dinner");
+
 
 // 👇🏽 Event Handlers & Functions 👇🏽
 // console.log("Hello world");
@@ -55,12 +57,15 @@ homeBtn.addEventListener("click", showHomeScreen);
 savedRecipeBtn.addEventListener("click", showSavedRecipes);
 pantryBtn.addEventListener("click", showPantry);
 recipeTile.addEventListener("click", viewRecipe);
-//recipePage.addEventListener("click", viewRecipe);
+checkboxes.forEach((box) => {
+  box.checked = false;
+  box.addEventListener("change", () => displayFiltered(recipeRepo))
+});
+recipePage.addEventListener("click", viewRecipe);
 
 // 👇🏽 Filter recipes by tag
 function recipeByCategory(tag) {
   //if receipeData.tag includes desired tag, then push recipe to recipeRepo.filtered (empty array in recipeRepository)
-
   recipeRepo.filterTag(tag);
   return recipeRepo.filtered;
 }
@@ -74,7 +79,7 @@ savedRecipeBtn.addEventListener("click", showSavedRecipes);
 pantryBtn.addEventListener("click", showPantry);
 
 function showAllRecipes() {
-  const hideElements = [homeView, allRecipeBtn, savedRecipesView];
+  const hideElements = [homeView, allRecipeBtn, savedRecipesView, recipePage];
   const showElements = [allRecipesView, homeBtn, savedRecipeBtn];
   hideElements.forEach((element) => element.classList.add("hidden"));
   showElements.forEach((element) => element.classList.remove("hidden"));
@@ -130,7 +135,7 @@ function addRecipeCards() {
   //allRecipeGrid.innerHTML += recipeTile;
   //return newRecipeCard;
 }
-addRecipeCards(recipeData);
+addRecipeCards();
 
 function viewRecipe(ev) {
   //console.log("recipeData", recipeData[1].id);
@@ -145,7 +150,7 @@ function viewRecipe(ev) {
     if (recipe.id === targetRecipeId) {
       const recipeInfo = recipeRepo.getById(targetRecipeId);
       const currentRecipe = new Recipe(recipeInfo);
-      recipePage.innerHTML += `<h1>${recipe.name}</h1>
+      recipePage.innerHTML = `<h1>${recipe.name}</h1>
       <img src="${recipe.image}">
       <h4>
         <ol>
@@ -157,7 +162,7 @@ function viewRecipe(ev) {
         </ol>
       </h4>
       <h4>${currentRecipe.getIngredients(ingredientsData)}</h4>
-      <h4>${currentRecipe.getCost(ingredientsData)}</h4>`;
+      <h4>${currentRecipe.getCost()}</h4>`;
     }
     //console.log(singleRecipeInstructions);
     //return singleRecipeInstructions;
@@ -165,16 +170,48 @@ function viewRecipe(ev) {
   });
   //console.log("jgj", singleRecipeInstructions);
 }
+let checked = []
+function grabCheckboxValues() {
+  checked = []
+  checkboxes.forEach((checkbox) => {
+    if (checkbox.checked) checked.push(checkbox.id)
+  })
+  console.log(checked)
+  return checked
+}
+
+function returnFiltered(repo) {
+repo.filtered = []
+grabCheckboxValues()
+console.log(checked)
+recipeTile.innerHTML = ``
+checked.forEach(value => {
+repo.filterTag(value)})
+console.log(repo.filtered)
+return repo.filtered
+}
+
+function displayFiltered(repo) {
+returnFiltered(repo)
+let filteredRecipes = repo.filtered.forEach((recipe) => {
+recipeTile.innerHTML += 
+ `<input type="image" src="${recipe.image}" id="${recipe.id}"/><h3>"${recipe.name}"</h3>`;
+  });
+  console.log(filteredRecipes)
+  return filteredRecipes;
+}
+
+
+
+
 
 // console.log("recipeCard", recipeCard);
 // console.log(addRecipeCard(recipeData[0]));
 
 /*
-
 // As a user, I should be able to view a list of all recipes.
 // show list of recipes
 // ----we want this to display the grid of images and recipe names
-
 // As a user, I should be able to click on a recipe to view more information including directions, ingredients needed, and total cost.
 // We need to build a card/page to show this information
 // Do we want it to have a name above image, above ingredients, above directions
@@ -182,16 +219,13 @@ function viewRecipe(ev) {
 // ---- Make new card with title, image, and directions
 // ---- Updates left menu to hide filters, but show ingredients needed for this recipe
 // ---- do we want
-
 // Do we want to build a pantry page that has updatable values for what the user has?
 // ---- Pantry button displays message that it is under construction
-
 // As a user, I should be able to add/remove a recipe to a list of recipes to cook
 // click saved recipes
 // hide main page
 // show grid of saved recipes
 // functionality to save or delete, similar to book covers
-
 //Make a class for saved recipes?
 //Add save recipe button to recipe card.
 //Save button adds save class to recipe card.
@@ -199,16 +233,9 @@ function viewRecipe(ev) {
 //If recipe card id contains "save" push to array.
 //Delete button will search saved array for recipe id and remove it.
 //Update saved grid
-
-
-
 // addRecipeCard can be invoked by clicking breakfast, lunch, dinner, or all recipes, with b.l.d. having pre-selected filter
-
 eventlistener on breakfast invokes addRecipeCard with filter applied.
 view all recipes doesnt have a filter applied
-
 to add to saved array, we need button to save recipe at bottom of recipe page, adds a saved class, and change add button to remove button, which will remove saved class
-
 saved recipe grid will filter to only show recipes with saved class when we hit saved recipes button
-
 */
