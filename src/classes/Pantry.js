0aaffa686f7ecdf190d118824a58cde4fc8b82b6
Shauna MacFarlane-Okongo <shauna.macfarlane579@gmail.com>
2022-07-28 22:13:23 -0500

    Refactor pantry class

--- src/classes/Pantry.js
+++ src/classes/Pantry.js
@@ -1,21 +1,17 @@
 class Pantry {
     constructor(user) {
         this.ingredientsOwned = user.pantry;
-        console.log('this.ingredientsOwned: ', this.ingredientsOwned)
         this.ingredientsToBuy = [];
-        this.ingredientsToCook = [];
     }
 checkForIngredients(id, recipeData) {
     const currentRecipe = recipeData.find(recipe => recipe.id === id).ingredients;
-    console.log('currentRecipe: ', currentRecipe)
     currentRecipe.forEach((ingredient) => {
         if (ingredient.id !== this.ingredientsOwned.ingredient) {
             this.ingredientsToBuy.push(ingredient.id)
         }
     })
-    console.log('ingredientsToBuy: ', this.ingredientsToBuy)
     return this.ingredientsToBuy
-}
+    }
 } 
 
 
