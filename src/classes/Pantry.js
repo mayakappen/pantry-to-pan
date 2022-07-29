3df0249dad20b1de63617179f1c05ee683b03a46
Dinne Kopelevich <dinne22@hotmail.com>
2022-07-29 12:26:12 -0600

    Add form

--- src/classes/Pantry.js
+++ src/classes/Pantry.js
@@ -1,18 +1,19 @@
 class Pantry {
-    constructor(user) {
-        this.ingredientsOwned = user.pantry;
-        this.ingredientsToBuy = [];
-    }
-checkForIngredients(id, recipeData) {
-    const currentRecipe = recipeData.find(recipe => recipe.id === id).ingredients;
+  constructor(user) {
+    this.ingredientsOwned = user.pantry;
+    this.ingredientsToBuy = [];
+  }
+  checkForIngredients(id, recipeData) {
+    const currentRecipe = recipeData.find(
+      (recipe) => recipe.id === id
+    ).ingredients;
     currentRecipe.forEach((ingredient) => {
-        if (ingredient.id !== this.ingredientsOwned.ingredient) {
-            this.ingredientsToBuy.push(ingredient.id)
-        }
-    })
-    return this.ingredientsToBuy
-    }
-} 
-
+      if (ingredient.id !== this.ingredientsOwned.ingredient) {
+        this.ingredientsToBuy.push(ingredient.id);
+      }
+    });
+    return this.ingredientsToBuy;
+  }
+}
 
 export default Pantry;
