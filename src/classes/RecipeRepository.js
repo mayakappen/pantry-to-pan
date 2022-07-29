199a44f68922ea52febbab020b3164eeab555463
Maya Kappen <mayakappen@gmail.com>
2022-07-29 15:23:15 -0500

    disregard capitals in filterName method, interpolate html correctly in filterByName function

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -11,11 +11,17 @@ class RecipeRepository {
     return this.filtered;
   }
   filterName(name) {
-    const recipe = this.recipes.find((value) => {
-      return value.name.includes(name);
-    });
-    return recipe;
+    let upper = name.toUpperCase()
+    let results = this.recipes.map(recipe => recipe.name).map(recipe => recipe.toUpperCase())
+    let result = results.findIndex(element => {
+      if (element === upper) {
+        return true
+      }
+      return false
+    })
+    return this.recipes[result]
   }
+  
   getById(id) {
     const recipe = this.recipes.find((recipe) => {
       return recipe.id === id;
